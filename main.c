f757e700bb39f9753e3e8081dfaa3a4f3fe6dfbc
Ethan-Pongon <48189446+Ethan-Pongon@users.noreply.github.com>
2019-03-08 16:02:06 -0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,42 +1,34 @@
-#include<stdio.h>
-
-/* 
-
+/*
+Ethan Pongon
 Ryan Whittingham
 Trevor Holland
-Ethan Pongon
-
-CSE222-PA5: The Knapsack Problem
------------------------------------------------
-PA5 is a C program that calculates the maximum 
-value a knapsack of a given size can hold of 
-specific items.
------------------------------------------------
-
+PA5 - Knapsack Problem
 */
 
-void main(int argc, char** argv){ //Main function that accepts a command line argument
-	
-	int sackCapacity, ret;
-	char buffer[120];
+#include "custom.h"
 
-        if (argc == 1){ //No command line argument detected
-                printf("Input knapsack capacity: ");
-                fgets(buffer, 120, stdin);
-                ret=sscanf(buffer, "%d", &sackCapacity);
-	}
-        else if(argc != 2){ //Invalid # of command line arguments
-                printf("Invalid input.\n");
-		return;
-        }
-        else{ //Correct number of arguments
-                ret = sscanf(argv[1], "%d", &sackCapacity);
-        }
-	if (ret != 1 || sackCapacity < 1 || sackCapacity > 1024){ //Invalid user input
-		printf("Invalid input.\n");
-		return;
-	}
-	printf("Knapsack capacity is %d\n", sackCapacity); //Valid user input
-	
-	return;
+// I wrote this without implementing the argc and argv stuff so we just need to switch out the capacity variable with that.
+void main(){
+  int capacity = 12;
+  FILE* fp;
+  char buffer[120];
+  int i = 0;
+  fp = fopen("knapsack.data", "r");
+  while(fgets(buffer,120,fp) != NULL){
+    sscanf(buffer, "%d %d %s", &items[i].wt, &items[i].val, items[i].name);
+    i = i + 1;
+  }  
+  fclose(fp);
+
+  struct result final = maxval(capacity);
+
+  int j = 0;
+  printf("There are ");
+  while(j <  128){
+    if(final.itemcount[j] != 0){
+      printf("%d %s, ", final.itemcount[j], items[j].name);
+    }
+    j = j + 1;
+  }
+  printf("in the sack \n");
 }
