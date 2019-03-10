3bc3b1fd938552c427c006499aee3fa9f8b23769
ptrevordactyl <45410698+ptrevordactyl@users.noreply.github.com>
2019-03-10 15:51:42 -0700

    Fixed error with missing/poorly written knapsack.data

--- sackinfo.c
+++ sackinfo.c
@@ -13,12 +13,24 @@ int sackinfo()
 	FILE* fp;
 	char buffer[120];
 	int i=0;
-	fp=fopen("knapsack.data","r"); //open file for reading
-	while(fgets(buffer,120,fp) != NULL) //read until end of file
+	if(access("knapsack.data", R_OK) != -1)
 	{
-		sscanf(buffer,"%d %d %s",&items[i].wt, &items[i].val, items[i].name); //read weight, value, and name of item from file. "sack" is a global variable
-		i++; //count number of item types
+		fp=fopen("knapsack.data","r"); //open file for reading
+		while(fgets(buffer,120,fp) != NULL) //read until end of file
+		{
+			if(3 == sscanf(buffer,"%d %d %s",&items[i].wt, &items[i].val, items[i].name)) i++; //read weight, value, and name of item from file. "sack" is a global variable. i counts number of item types
+			else //knapsack.data is structured incorrectly
+			{
+				printf("ERROR: INVALID DATA IN knapsack.data\n");
+				exit(0);
+			}
+		}
+		fclose(fp);
+		return i;
 	}
-	fclose(fp);
-	return i;
+	else
+	{
+		printf("ERROR, THE FILE knapsack.data DOES NOT EXIST\n");
+		exit(0);
+	}
 }
